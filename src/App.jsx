d9abf61a043de076e521a9d395d3b2a86babe340
JoshuaMendez <Joshuamendez0501@gmail.com>
2024-09-16 19:30:45 -0500

    a fresh start

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx'
 import Leftbar from './components/Leftbar/Leftbar.jsx'
 import Blog from './components/blog/blog.jsx'
-// import { BarraNavegacionSuperior } from './components/barra-navegacion-superior/barra-navegacion-superior.jsx';
+// import Share from './components/Share/Share.jsx'
 
 function App() {
   return (
     <div className='container'>
       <Navbar />
       <Leftbar />
-      <Blog />
+      <div className='columns'>
+        <div className='column'>
+          <Blog />
+        </div>
+        <div className='column'>
+          {/* <Share /> */}
+        </div>
+        <div className='column'>
+          <Blog />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -6,8 +6,8 @@ import '../../assets/icons/css/bb-icons.css'; // Ensure this path is correct
 const Blog = () => {
     return (
         <div class = "bloque-blog">
-            <h2 id = "title-blog">Blog</h2>
-            <button class = "boton-blog">See all</button>
+            <h2>Blog</h2>
+            <button>See all</button>
         </div>
     );
 }
