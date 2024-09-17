a6049233b101da1b9308049710d66b2c9c2eb6ac
JoshuaMendez <Joshuamendez0501@gmail.com>
2024-09-16 22:23:01 -0500

    add share

--- src/components/Share/Share.jsx
+++ src/components/Share/Share.jsx
@@ -8,33 +8,32 @@ function App() {
             <div className="shareTop">
             <img className="shareProfileImg" src={man} alt="Profile"/>
             <input
-                placeholder="Share what's on your mind, John"
+                placeholder="Share what's on your mind, John..."
                 className="shareInput"
             />
             </div>
-            <hr className="shareHr"/>
-            <div className="shareBottom">
-                <div className="shareOptions">
-                    <div className="shareOption">
-                        <span className="bb-icon-f bb-icon-camera shareIcon"></span>
-                    </div>
-                    <div className="shareOption">
-                        <span className="bb-icon-f bb-icon-video shareIcon"></span>
-                    </div>
-                    <div className="shareOption">
-                        <span className="bb-icon-f bb-icon-attach shareIcon"></span>
-                    </div>
-                    <div className="shareOption">
-                        <span className="bb-icon-f bb-icon-gif shareIcon"></span>
-                    </div>
-                    <div className="shareOption">
-                        <span className="bb-icon-f bb-icon-poll shareIcon"></span>
-                    </div>
+        </div>
+        <div className="shareBottom">
+            <div className="shareOptions">
+                <div className="shareOption">
+                    <span className="bb-icon-f bb-icon-camera shareIcon"></span>
                 </div>
-                <button className="shareButton">Share</button>
+                <div className="shareOption">
+                    <span className="bb-icon-f bb-icon-video shareIcon"></span>
+                </div>
+                <div className="shareOption">
+                    <span className="bb-icon-f bb-icon-attach shareIcon"></span>
+                </div>
+                <div className="shareOption">
+                    <span className="bb-icon-f bb-icon-gif shareIcon"></span>
+                </div>
+                <div className="shareOption">
+                    <span className="bb-icon-f bb-icon-poll shareIcon"></span>
+                </div>
             </div>
+            <button className="bb-icon-f bb-icon-location-arrow shareButton"></button>
         </div>
-        </div>
+    </div>
     );
 }
 
