6b5e9df4d6cf32c4bef74c60a85d98dea0bd566a
sebasizq <sebastiansaavedra_2005@hotmail.com>
2024-09-19 01:27:28 -0500

    add courses

---
+++ src/components/curso/curso.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import './curso.css';
+import '../../assets/icons/css/bb-icons.css';
+
+
+
+const Curso = ({portada,titulo, cantidadLessons}) => {
+    return (
+        <div class = "bloque-curso">
+            <div class = "portada-curso">
+                <img src ={portada}></img>
+            </div>
+            <div class = "informacion-curso">
+                <div className="curso-detalles">
+                    <span className="curso-lecciones">{cantidadLessons} Lecciones</span>
+                    <h2 className="curso-titulo">{titulo}</h2>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Curso;
--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -9,14 +9,13 @@ import Post from '../../components/Post/Post.jsx'
 import Feed from '../../components/Feed/Feed.jsx'
 import LatestUpdates from '../../components/LatestUpdates/latestUpdates.jsx'
 import Following from '../../components/following/following.jsx'
-
+import Curso from '../../components/curso/curso.jsx'
 
 function Courses() {
     return (
         <div className='container-courses'>
         <Navbar />
-        <Leftbar />
-        
+        <Curso portada="https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2024/07/19/halo-p.jpeg" titulo="HALO 1 PARA PENDEJOS" cantidadLessons={10}/>
         </div>
     );
 }
