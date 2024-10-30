a25f45f0a87c9d3af291faa0b0141b248d948c76
sebasizq <sebastiansaavedra_2005@hotmail.com>
2024-10-30 15:08:45 -0500

    componente photos

--- src/pages/photos/photo.jsx
+++ src/pages/photos/photo.jsx
@@ -34,7 +34,30 @@ function Photo() {
                         <SubProfile />
                     </div>
                     <div className="general-profile-container-photo">
-                        Photos..
+                        <div class = "ctr-photos">
+                            <div class ="ctr-head">
+                                <h3>Photos</h3>
+                                <div class="btn-photos"> 
+                                    <h4>Add photos</h4>
+                                </div>
+                            </div>
+                            <div class="nav-photos"> 
+                                <div class="btn-photos btn-active"> 
+                                    <h4>Photos</h4>
+                                </div>
+                                <div class="btn-photos"> 
+                                    <h4>Albums</h4>
+                                </div>
+                            </div>
+                            <div class = "collegue-photos">
+                                <img src = "https://media.licdn.com/dms/image/v2/D4E03AQFeha-NvXWoDw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715274045600?e=1735776000&v=beta&t=WF-AIf4v5qx-j7TzZQCYtZB2EaWcxFqIPqL-JD4st0g"></img>
+                                <img src = "https://media.licdn.com/dms/image/v2/D4E03AQFeha-NvXWoDw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715274045600?e=1735776000&v=beta&t=WF-AIf4v5qx-j7TzZQCYtZB2EaWcxFqIPqL-JD4st0g"></img>
+                                <img src = "https://media.licdn.com/dms/image/v2/D4E03AQFeha-NvXWoDw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715274045600?e=1735776000&v=beta&t=WF-AIf4v5qx-j7TzZQCYtZB2EaWcxFqIPqL-JD4st0g"></img>
+                                <img src = "https://media.licdn.com/dms/image/v2/D4E03AQFeha-NvXWoDw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715274045600?e=1735776000&v=beta&t=WF-AIf4v5qx-j7TzZQCYtZB2EaWcxFqIPqL-JD4st0g"></img>
+                                <img src = "https://media.licdn.com/dms/image/v2/D4E03AQFeha-NvXWoDw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715274045600?e=1735776000&v=beta&t=WF-AIf4v5qx-j7TzZQCYtZB2EaWcxFqIPqL-JD4st0g"></img>
+                                <img src = "https://media.licdn.com/dms/image/v2/D4E03AQFeha-NvXWoDw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715274045600?e=1735776000&v=beta&t=WF-AIf4v5qx-j7TzZQCYtZB2EaWcxFqIPqL-JD4st0g"></img>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="spacer-column-profile-photo">
