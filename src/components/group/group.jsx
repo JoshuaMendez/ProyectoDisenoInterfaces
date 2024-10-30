1176d78db899f08d682dcffdbb334b198ddce90b
sebasizq <sebastiansaavedra_2005@hotmail.com>
2024-10-30 12:08:47 -0500

    componente grupos

---
+++ src/components/group/group.jsx
@@ -0,0 +1,37 @@
+import './group.css';
+import '../../assets/icons/css/bb-icons.css';
+
+function Group({name, perfil, portada, tipo, foto1, foto2, foto3}) {
+    return (
+    <div class="profile-container">
+        <div class="profile-header">
+            <img src = {portada}  class="profile-banner"/>
+            <div class="profile-ctn">
+                <div class="profile-img-status">
+                    <img src = {perfil} class="profile-pic"/>
+                </div>
+            </div>
+            <div class="profile-data">
+            <h2 class="nameGroup">{name}</h2>
+            <span>{tipo} • joined </span>
+            </div>
+        </div>
+        <div class = "group-down-part">
+            <div class="group-participants">
+                <img src = {foto1} class="profile-pic-down"/>
+                <img src ={foto2} class="profile-pic-down"/>
+                <img src = {foto3} class="profile-pic-down"/>
+                <i className="bb-icon-ellipsis-h"></i>
+            </div>
+            <div class="group-btn"> 
+                <div class = "botonDown">
+                    <i className="bb-icon-check"></i>
+                    <h4>Organizer</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Group;
