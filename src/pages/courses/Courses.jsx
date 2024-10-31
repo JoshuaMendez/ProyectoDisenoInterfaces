70b40828efa6ea1a05027ed9361bfd8a07a980a9
july123la <july123la@gmail.com>
2024-10-31 00:37:01 -0500

    estado global del perfil

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -15,17 +15,6 @@ function Courses() {
                 {/* Contenido de la primera columna */}
             </div>
             <div className="middle-column-profile-course">
-                <div className="profile-container-course">
-                    <UserProfileComponent
-                        name='John' 
-                        followers='15' 
-                        following='3' 
-                        joined='last week' 
-                        social='Admin' 
-                        image='https://online-communities.demos.buddyboss.com/wp-content/sandbox334681-uploads/avatars/2/621e2ce403550-bpfull.png   ' 
-                        portada='https://online-communities.demos.buddyboss.com/wp-content/sandbox334681-uploads/buddypress/members/2/cover-image/621e2cf2a9e5d-bp-cover-image.jpg'
-                    />
-                </div>
                 <div className="menu-subprofile-course">
                     <SubProfile />
                 </div>
