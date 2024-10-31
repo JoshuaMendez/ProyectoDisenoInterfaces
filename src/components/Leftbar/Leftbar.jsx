60d5b1c2fce88db437a6aa8a0e041e8f790ccc26
july123la <july123la@gmail.com>
2024-10-31 03:07:37 -0500

    leftbar y subprofile

--- src/components/Leftbar/Leftbar.jsx
+++ src/components/Leftbar/Leftbar.jsx
@@ -1,7 +1,7 @@
 // Leftbar.jsx
 import React from 'react';
 import './Leftbar.css';
-import '../../assets/icons/css/bb-icons.css'; // Ensure this path is correct
+import '../../assets/icons/css/bb-icons.css'; // Asegúrate de que esta ruta sea correcta
 import { Link } from 'react-router-dom'; 
 import { useSelector } from 'react-redux';
 
@@ -11,105 +11,111 @@ const Leftbar = () => {
     const handleToggle = () => {
         setIsExpanded(!isExpanded);
     };
+
     return (
         <nav>
             {isExpanded && (
-                <div className = "barExpandida">
-                    <div id = "header-desplegable">
+                <div className="barExpandida">
+                    <button id="header-desplegable">
                         <i className="bb-icon-sidebar" id="desplegable" onClick={handleToggle}></i>
-                    </div>
+                    </button>
                     <div className='rellenoBarExpandida'>
                         <ul className='icon-list-left'>
-                        <h4>PERSONAL</h4>
-                        <li className={currentPage === '/profile' ? "filled": "clear"}>
-                            <Link to="/profile">
-                                <i className={currentPage === '/profile' ? "bb-icon-user-filled" :"bb-icon-user"}></i><span>My Profile</span>
-                            </Link>
-                        </li>
-                        <li className={currentPage === '/timeline' ? "filled": "clear"}>
-                            <Link to="/timeline">
-                                <i className="bb-icon-activity"></i><span>My Timeline</span>
-                            </Link>
-                        </li>
-                        
-                        <h4>COMMUNITY</h4>
-                        <li className={currentPage === '/groups' ? "filled": "clear"}>
-                            <Link to="/groups">
-                            <i className="bb-icon-users"></i><span>My Groups</span>
-                            </Link>
-                        </li>
-                        <li className={currentPage === '/messages' ? "filled": "clear"}>
-                            <Link to="/messages">
-                            <i className="bb-icon-comments-square"></i><span>My Discussions</span>
-                            </Link>
-                        </li>     
-                        <li className={currentPage === '/courses' ? "filled": "clear"}>
-                            <Link to="/courses">
-                                <i className="bb-icon-graduation-cap"></i><span>My Courses</span>
-                            </Link>
-                        </li>
-                        <h4>MEDIA</h4> 
-                        <li className={currentPage === '/photo' ? "filled": "clear"}>
-                            <Link to="/photo">
-                                <i className="bb-icon-image"></i><span>My Photos</span>
-                            </Link>
-                        </li>
-                        <li className={currentPage === '/documents' ? "filled": "clear"}>
-                            <Link to="/documents">
-                                <i className="bb-icon-folder-alt"></i><span>My Documents</span>
-                            </Link>
-                        </li>      
+                            <h4>PERSONAL</h4>
+                            <li className={currentPage === '/profile' ? "filled" : "clear"}>
+                                <Link to="/profile">
+                                    <i className={currentPage === '/profile' ? "bb-icon-f bb-icon-user icon-colored" : "bb-icon-l bb-icon-user"}></i>
+                                    <span>My Profile</span>
+                                </Link>
+                            </li>
+                            <li className={currentPage === '/timeline' ? "filled" : "clear"}>
+                                <Link to="/timeline">
+                                    <i className="bb-icon-activity"></i>
+                                    <span>My Timeline</span>
+                                </Link>
+                            </li>
+                            <h4>COMMUNITY</h4>
+                            <li className={currentPage === '/groups' ? "filled" : "clear"}>
+                                <Link to="/groups">
+                                    <i className="bb-icon-users"></i>
+                                    <span>My Groups</span>
+                                </Link>
+                            </li>
+                            <li className={currentPage === '/messages' ? "filled" : "clear"}>
+                                <Link to="/messages">
+                                    <i className="bb-icon-comments-square"></i>
+                                    <span>My Discussions</span>
+                                </Link>
+                            </li>     
+                            <li className={currentPage === '/courses' ? "filled" : "clear"}>
+                                <Link to="/courses">
+                                    <i className="bb-icon-graduation-cap"></i>
+                                    <span>My Courses</span>
+                                </Link>
+                            </li>
+                            <h4>MEDIA</h4> 
+                            <li className={currentPage === '/photos' ? "filled" : "clear"}>
+                                <Link to="/photos">
+                                    <i className="bb-icon-image"></i>
+                                    <span>My Photos</span>
+                                </Link>
+                            </li>
+                            <li className={currentPage === '/documents' ? "filled" : "clear"}>
+                                <Link to="/documents">
+                                    <i className="bb-icon-folder-alt"></i>
+                                    <span>My Documents</span>
+                                </Link>
+                            </li>      
                         </ul>
                     </div>
                 </div>
             )}
-                    {!isExpanded && (
-                    <div className='leftbar'>
-                        <div id = "header-desplegable">
-                            <i className="bb-icon-sidebar" id="desplegable" onClick={handleToggle}></i>
-                        </div>
-                        <ul className='icon-list-left'>
-                        <li>
-                            <Link to = "/profile">
+            {!isExpanded && (
+                <div className='leftbar'>
+                    <button id="header-desplegable">
+                        <i className="bb-icon-sidebar" id="desplegable" onClick={handleToggle}></i>
+                    </button>
+                    <ul className='icon-list-left'>
+                        <li className={currentPage === '/profile' ? "filled" : "clear"}>
+                            <Link to="/profile">
                                 <i className="bb-icon-user"></i>
                             </Link>
                         </li>
-                        <li>
-                            <Link to = "/timeline">
+                        <li className={currentPage === '/timeline' ? "filled" : "clear"}>
+                            <Link to="/timeline">
                                 <i className="bb-icon-activity"></i>
                             </Link>
                         </li>
                         <li><div className='divider-horizontal'></div></li>
-                        <li>
-                            <Link to ="/groups">
+                        <li className={currentPage === '/groups' ? "filled" : "clear"}>
+                            <Link to="/groups">
                                 <i className="bb-icon-users"></i>
                             </Link>
                         </li>
-                        <li>
+                        <li className={currentPage === '/messages' ? "filled" : "clear"}>
                             <Link to="/messages">
-                            <i className="bb-icon-comments-square"></i>
+                                <i className="bb-icon-comments-square"></i>
                             </Link>
                         </li>   
-                        <li>
+                        <li className={currentPage === '/courses' ? "filled" : "clear"}>
                             <Link to="/courses">
-                                 <i className="bb-icon-graduation-cap"></i>
+                                <i className="bb-icon-graduation-cap"></i>
                             </Link>     
                         </li> 
                         <li><div className='divider-horizontal'></div></li>  
-                        <li>
+                        <li className={currentPage === '/photos' ? "filled" : "clear"}>
                             <Link to="/photos"> 
                                 <i className="bb-icon-image"></i>
                             </Link>    
                         </li>
-                        <li>
-                            <Link to = "/documents">
+                        <li className={currentPage === '/documents' ? "filled" : "clear"}>
+                            <Link to="/documents">
                                 <i className="bb-icon-folder-alt"></i>
                             </Link>
                         </li>     
-                        </ul>
-                    </div>
-                )}         
-            
+                    </ul>
+                </div>
+            )}         
         </nav>
     );
 }
--- src/components/SubProfile/SubProfile.jsx
+++ src/components/SubProfile/SubProfile.jsx
@@ -3,38 +3,40 @@ import React from 'react';
 import { Link } from 'react-router-dom'; 
 import './SubProfile.css';
 import '../../assets/icons/css/bb-icons.css';
+import { useSelector } from 'react-redux';
 
 const SubProfile = () => {
+    const currentPage = useSelector((state) => state.page.currentPage);
     return (
         <div className='menu-subprofile-component'>
             <li>
                 <Link to="/timeline">
-                    <i className="in-subprofile-list">Timeline</i>
+                    <i className={currentPage === '/timeline' ? "in-subprofile-list-strong" : "in-subprofile-list"}>Timeline</i>
                 </Link>
             </li>
             <li>
                 <Link to="/profile">
-                    <i className="in-subprofile-list">Profile</i>
+                    <i className={currentPage === '/profile' ? "in-subprofile-list-strong" : "in-subprofile-list"}>Profile</i>
                 </Link>
             </li>
             <li>
                 <Link to="/groups">
-                    <i className="in-subprofile-list">Groups</i>
+                    <i className={currentPage === '/groups' ? "in-subprofile-list-strong" : "in-subprofile-list"}>Groups</i>
                 </Link>
             </li>
             <li>
                 <Link to="/courses">
-                    <i className="in-subprofile-list">Courses</i>
+                    <i className={currentPage === '/courses' ? "in-subprofile-list-strong" : "in-subprofile-list"}>Courses</i>
                 </Link>
             </li>
             <li>
                 <Link to="/documents">
-                    <i className="in-subprofile-list">Documents</i>
+                    <i className={currentPage === '/documents' ? "in-subprofile-list-strong" : "in-subprofile-list"}>Documents</i>
                 </Link>
             </li>
             <li>
                 <Link to="/photos">
-                    <i className="in-subprofile-list">Photos</i>
+                    <i className={currentPage === '/photos' ? "in-subprofile-list-strong" : "in-subprofile-list"}>Photos</i>
                 </Link>
             </li>
         </div>
