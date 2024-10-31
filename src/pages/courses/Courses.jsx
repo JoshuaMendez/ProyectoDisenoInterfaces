1353fc41ffee7ef762d77237305e3cb79819d4ea
sebasizq <sebastiansaavedra_2005@hotmail.com>
2024-10-31 13:50:36 -0500

    correciones

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -9,7 +9,7 @@ function Courses() {
             <div className="middle-column-profile-course">
                 <div className="general-profile-container-course">
                     <div className="container-course">
-                        <Curso portada="https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2024/07/19/halo-p.jpeg" titulo="HALO 1 PARA PENDEJOS" cantidadLessons={10}/>
+                        <Curso portada="https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2024/07/19/halo-p.jpeg" titulo="HALO 1" cantidadLessons={10}/>
                     </div>
                     <div className="container-course">
                         <Curso portada="https://th.bing.com/th/id/R.4bb42aa1ee4f35ffcc637113f77fd945?rik=pcFQwdexbos%2b5Q&riu=http%3a%2f%2frecla.org%2fwp-content%2fuploads%2f2023%2f04%2fel-arte-generado-por-inteligencia-artificial-transformara-la-educacion-en-las-facultades-de-diseno-grafico-scaled.jpg&ehk=mwOLSxpdbwEwm41mahCi2omb8TaxybR%2fv2YivhMA4ys%3d&risl=&pid=ImgRaw&r=0" titulo="DiSEÑO DIGITAL PARA PRINCIPIANTES" cantidadLessons={14}/>
--- src/pages/groups/groups.jsx
+++ src/pages/groups/groups.jsx
@@ -9,31 +9,31 @@ function Groups() {
                     <div className="middle-column-profile-photo">
                         <div className="general-profile-container-college">
                             <Group
-                            name = "Juliana lovers"
-                            perfil="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
-                            foto1="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
-                            foto2="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
-                            foto3="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
+                            name = "MMA lovers"
+                            perfil="https://a.espncdn.com/i/headshots/mma/players/full/3022677.png"
+                            foto1="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/0ea5c-princesa-jazmin-disney-png.png?fit=500%2C500&ssl=1"
+                            foto2="https://w7.pngwing.com/pngs/631/446/png-transparent-disney-princess-illustration-belle-princess-aurora-cinderella-rapunzel-princess-jasmine-beauty-and-the-beast-disney-princess-beast-doll-thumbnail.png"
+                            foto3="https://w7.pngwing.com/pngs/23/472/png-transparent-princess-aurora-princess-jasmine-disney-princess-sleeping-beauty-aurora-princess-thumbnail.png"
                             tipo = "Public"
-                            portada = "https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
+                            portada = "https://s3.sportstatics.com/relevo/www/multimedia/202210/05/media/cortadas/lozanoafl-RUQ4DDnR5W83otTnBP2qCcM-1200x648@Relevo.jpg"
                             />
                                 <Group
-                            name = "Juliana lovers"
-                            perfil="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
-                            foto1="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
-                            foto2="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
-                            foto3="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
+                            name = "AntiPetro"
+                            perfil="https://preview.redd.it/fuera-petro-fuera-asesino-v0-9l3h46mnm2kd1.jpeg?auto=webp&s=3e10800023a0b3320b5cf0ecff1a23c6ecadb0b9"
+                            foto1="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/0ea5c-princesa-jazmin-disney-png.png?fit=500%2C500&ssl=1"
+                            foto2="https://w7.pngwing.com/pngs/631/446/png-transparent-disney-princess-illustration-belle-princess-aurora-cinderella-rapunzel-princess-jasmine-beauty-and-the-beast-disney-princess-beast-doll-thumbnail.png"
+                            foto3="https://w7.pngwing.com/pngs/23/472/png-transparent-princess-aurora-princess-jasmine-disney-princess-sleeping-beauty-aurora-princess-thumbnail.png"
                             tipo = "Public"
-                            portada = "https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
+                            portada = "https://cloudfront-us-east-1.images.arcpublishing.com/semana/ZBLN3LPTOBG6ZNLBZL7VHE55AE.jpg"
                             />
                                 <Group
-                            name = "Juliana lovers"
-                            perfil="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
-                            foto1="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
-                            foto2="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
-                            foto3="https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
+                            name = "El mejor Presidente"
+                            perfil="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbtZWANkz6zvVLB9EIXrCtuA1UjvBu3rcYdg&s"
+                            foto1="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/0ea5c-princesa-jazmin-disney-png.png?fit=500%2C500&ssl=1"
+                            foto2="https://w7.pngwing.com/pngs/631/446/png-transparent-disney-princess-illustration-belle-princess-aurora-cinderella-rapunzel-princess-jasmine-beauty-and-the-beast-disney-princess-beast-doll-thumbnail.png"
+                            foto3="https://w7.pngwing.com/pngs/23/472/png-transparent-princess-aurora-princess-jasmine-disney-princess-sleeping-beauty-aurora-princess-thumbnail.png"
                             tipo = "Public"
-                            portada = "https://www.cidob.org/sites/default/files/styles/max_width_290/public/2024-10/Putin.jpg.webp?itok=Oll3K22Y"
+                            portada = "https://imagenes.elpais.com/resizer/v2/U6RLCK5DBNAFFO2KKEHPXVWNOY.jpg?auth=da6a83131da7456288001b8d32b8d6b48d57ff45eabc194f449f1aa8a32e5943&width=1960&height=1103&focal=2035%2C1108"
                             />
                         </div>
                     </div>
