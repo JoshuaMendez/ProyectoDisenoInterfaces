f3fd4099117f1e3b18a3a1ce79a3801c6edc0bc1
JoshuaMendez <Joshuamendez0501@gmail.com>
2024-11-21 14:49:20 -0500

    connected to firebase

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,24 @@ import Leftbar from './components/Leftbar/Leftbar';
 import LatestUpdates from './components/LatestUpdates/latestUpdates';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCPEg_hMCzZ_nnGJX96OFb2zg_7-VyaLHk",
+    authDomain: "carelibro-ef121.firebaseapp.com",
+    projectId: "carelibro-ef121",
+    storageBucket: "carelibro-ef121.firebasestorage.app",
+    messagingSenderId: "664311706518",
+    appId: "1:664311706518:web:20bb21724336f97fe7a3b7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 function App() {
     const location = useLocation();
     const dispatch = useDispatch();
